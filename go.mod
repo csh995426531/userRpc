4af9b1d55f2f44182edc85f7681086adeccbc952
csh <1114313879@qq.com>
2021-08-16 10:19:10 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module userRpc
+
+go 1.15
---
+++ userrpc.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"userRpc/internal/config"
+	"userRpc/internal/server"
+	"userRpc/internal/svc"
+	"userRpc/userRpc"
+
+	"github.com/tal-tech/go-zero/core/conf"
+	"github.com/tal-tech/go-zero/zrpc"
+	"google.golang.org/grpc"
+)
+
+var configFile = flag.String("f", "etc/userrpc.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+	srv := server.NewUserRpcServer(ctx)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		userRpc.RegisterUserRpcServer(grpcServer, srv)
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
